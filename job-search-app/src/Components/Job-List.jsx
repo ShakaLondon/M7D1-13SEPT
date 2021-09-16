e45a4719652c7e6c3675aeb3c251f4fec23cf830
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-09-16 12:48:06 +0100

    Update Job-List.jsx

--- job-search-app/src/Components/Job-List.jsx
+++ job-search-app/src/Components/Job-List.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, setState } from 'react';
-import { Navbar, Container, NavDropdown, Nav, Button, InputGroup, FormControl, Row, Col, ListGroup } from 'react-bootstrap'
+import { Navbar, Container, NavDropdown, Nav, Button, InputGroup, FormControl, Row, Col, ListGroup, Card } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGripHorizontal } from '@fortawesome/free-solid-svg-icons'
 import { withRouter, Redirect } from 'react-router-dom';
@@ -13,21 +13,51 @@ function ResultList(props) {
     console.log(searchReq)
 
     const [searchRequest, setSearchRequest] = useState(searchReq);
-    const [searchResult, setSearchResult] = useState(searchReq);
+    const [searchResult, setSearchResult] = useState([]);
 
     useEffect(() => {
-        const url =
-        fetch(`${url}${searchRequest}`).then(
-            res => setState(res.data)
-        )
-    })
+        const url = `https://strive-jobs-api.herokuapp.com/jobs?search=`
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGIwZGZiYmRjMTQ1ODAwMTVlNGFlZTUiLCJpYXQiOjE2MzE3NzI3MTIsImV4cCI6MTYzMjk4MjMxMn0.2YWhQrKLUrKnO_spK_yPMr-orqdslBjHVr-zMEUyYPk'
+            }
+        }
+        fetch(`${url}${searchRequest}`, options)
+        .then(res => res.json())
+        .then((jobs) => {
+            console.log(jobs.data)
+            setSearchResult(jobs.data)
+            console.log(searchResult)
+        })
+        .catch((error) => {console.log(error)})
+    }, [])
+
+
 
   return (
       <Container className="d-flex" style={{height: "100%"}}>
         <Row style={{width: "100%"}}>
             <Col md={12} lg={12}>
                 <ListGroup>
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    
+                    { searchResult ? (
+                        searchResult.map((job) => 
+                            <ListGroup.Item className="border-0">
+                                <Card style={{ width: '100%' }}>
+                                    <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </ListGroup.Item>)) :
+                            (<h2>There are no results for your search! Try again!</h2>)
+                    }
+                    
                     <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
                     <ListGroup.Item>Morbi leo risus</ListGroup.Item>
                     <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
